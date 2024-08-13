9daa9ed828d37e01eb6bef43c0cf74090f706885
Choi Sunhong <123724883+Choisunhong@users.noreply.github.com>
2024-08-14 00:10:55 +0900

    [Silver III] Title: 2×n 타일링, Time: 40 ms, Memory: 31252 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+N = int(input().strip()) 
+res = [0]*(N+1)
+res[1]=1
+
+if N > 1:
+    res[2] = 2
+
+
+for i in range(3, N + 1):
+    res[i] = (res[i-1] + res[i-2]) % 10007
+
+
+print(res[N])
