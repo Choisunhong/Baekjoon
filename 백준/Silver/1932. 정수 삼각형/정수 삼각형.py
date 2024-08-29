30666cb6846633ebddaae5ca9ae5409b1ce43ca0
Choi Sunhong <123724883+Choisunhong@users.noreply.github.com>
2024-08-30 03:20:02 +0900

    [Silver I] Title: 정수 삼각형, Time: 112 ms, Memory: 41684 KB -BaekjoonHub

---
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.py
@@ -0,0 +1,17 @@
+import sys 
+input = sys.stdin.readline
+N = int(input())
+tri = []
+for _ in range(N):
+    row = list(map(int, input().split()))
+    tri.append(row)
+dp = [[0] * N for _ in range(N)]
+
+for i in range(N):
+    dp[N-1][i] = tri[N-1][i]
+
+for i in range(N-2, -1, -1):
+    for j in range(i+1):
+        dp[i][j] = max(dp[i+1][j], dp[i+1][j+1]) + tri[i][j]
+
+print(dp[0][0])
